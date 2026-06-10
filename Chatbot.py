c3a6d4587a4f7aa56819a41782a56f192bec577e
Phantomman2 <anthonybonilla786@gmail.com>
2026-06-10 12:55:01 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -5,6 +5,7 @@ st.title("simple chat")
 # intitialize chat history
 if "messages" not in st.session_state:
   st.session_state.messages=[]
+  
   # Display chat messages from history on app rerun 
   for message in st.session_state.messages:
     with st.chat_messsage(message["role"]):
@@ -15,6 +16,7 @@ if propmt := st.chat_input("what is up") :
   # Displau user messahe in chat message container
   with st.chat_message("user") :
     st.markdown(prompt)
+    
     # Add user message to chat history
     st.session_state.message.append({"role": "user", "content": prompt})
 
