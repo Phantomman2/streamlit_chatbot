f6599452f56afe698f1505e513018cc2c9c8dffc
Phantomman2 <anthonybonilla786@gmail.com>
2026-06-10 12:18:20 -0400

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -16,4 +16,23 @@ if propmt := st.chat_input("what is up") :
     st.markdown(prompt)
     # Add user message to chat history
     st.session_state.message.append({"role": "user", "content": prompt})
-    
+
+# streamed response emulator
+def response_generator() :
+  response = random.choice(
+    [
+      "Hello there! How can i assit uou today?",
+      "Hi, human! Is there amything i can help you with?",
+      Do you need help?"
+    ]
+  )
+  for word in response.split():
+    yield word +"  "
+    time.sleep (0.05)
+
+# Dislay assistant response in chat message container
+with st.chat_message("assistant") :
+ Response = st.write_stream(response_genrator())
+
+# Add assitant response to chat history
+st.session_state.messages.append({"role": "assostant", "content": response})
